c40dad5e5862f9768c4198e327a86b5c9b0599b4
LksRolls <lukasrolland57@gmail.com>
2026-03-04 15:30:43 +0100

    plus qu'a y croire V4

--- backend/start.sh
+++ backend/start.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 set -e
 
-# Le WORKDIR dans le Dockerfile est déjà /app/backend, 
-# mais on assure le coup
 cd /app/backend
 
 echo "--- Running Prisma migrations ---"
-# Utilisation de npx (plus léger ici) ou pnpm
 npx prisma migrate deploy
 
 echo "--- Starting NestJS Server ---"
-# Ajout de .js à la fin de main
 exec node dist/main.js
